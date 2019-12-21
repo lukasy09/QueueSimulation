8556f6f6819e583397ed5ae7b6730f107a0a297d
Łukasz <lukasy09@gmail.com>
2019-12-21 17:38:28 +0100

    reports done

--- src/ReportGenerator/xls_report.py
+++ src/ReportGenerator/xls_report.py
@@ -1,6 +1,7 @@
 import xlwt
 from xlwt import Workbook
 import json
+from datetime import datetime
 
 
 class XLSReportGenerator:
@@ -26,11 +27,19 @@ class XLSReportGenerator:
             self.sheets[1].write(0, queue_col, queue["type"])
             data_row = 1
             for j, key in enumerate(queue.keys()):
-                if key not in ["type" ,"queue", "active_customers_per_time"]:
+                if key not in ["type"]:
                     if i == 0:
                         self.sheets[1].write(data_row, 0, key)
                     self.sheets[1].write(data_row, queue_col, str(queue[key]))
                     data_row += 1
+
+    def write_customers(self, customers):
+        for i, customer in enumerate(customers):
+            for j, key in enumerate(customer.keys()):
+                if i == 0:
+                    self.sheets[2].write(0, j, key)
+
+                self.sheets[2].write(i+1, j, str(customer[key]))
 
     def build_report(self):
         with open(self.input_path, 'r') as input_filedata:
@@ -39,18 +48,29 @@ class XLSReportGenerator:
             # Creating sheets
             self.add_sheet('General statistics')
             self.add_sheet("Queues")
+            self.add_sheet("Customers")
 
             # Writing data
             stats = data['statistics']
             self.write_statistics(stats)
+
             queues = data['queues']
             self.write_queues(queues)
+
+            customers = data['customers']
+            self.write_customers(customers)
+
+
         self.submit()
 
-
     def submit(self):
-        self.wb.save(self.output_path)
+        final_path = "{}{}".format(self.output_path, self.generate_report_name())
+        self.wb.save(final_path)
+
+    def generate_report_name(self):
+        now = datetime.now()
+        return "report_{}.xls".format(str(now.strftime("%H-%M-%S")))
 
 
-gen = XLSReportGenerator("../../out/output.json", "./a.xls")
-gen.build_report()
+# gen = XLSReportGenerator("../../out/output.json", "./")
+# gen.build_report()
